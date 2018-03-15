0a38b089cea81ca26fbc8a671313856f56196dea
Anna <aavolkova>
2018-03-15 15:26:00 -0400

    Lesson 23. Using Current User Information

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -8,7 +10,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.security.Principal;
 
 @Controller
 public class HomeController {
@@ -27,10 +31,25 @@ public class HomeController {
         return "login";
     }
 
+    // Lesson 23 modifications:
     @RequestMapping("/secure")
-    public String secure(){
+    public String secure(HttpServletRequest request,
+                         Authentication authentication,
+                         Principal principal){
+
+        Boolean isAdmin = request.isUserInRole("ADMIN");
+        Boolean isUser = request.isUserInRole("USER");
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String username = principal.getName();
+
         return "secure";
     }
+
+
+//    @RequestMapping("/secure")
+//    public String secure(){
+//        return "secure";
+//    }
 
 //    @RequestMapping("/admin")
 //    public String admin(){
